7a3145d03c3bcf234aa03d774fd71b7f5bcc5d2d
Craigthomaskey <craigthomaskey@gmail.com>
2018-12-17 16:17:34 -0600

    Again?

--- TransitFeeds/TransitFeeds/Form1.Designer.cs
+++ TransitFeeds/TransitFeeds/Form1.Designer.cs
@@ -29,45 +29,21 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.MainFeed = new System.Windows.Forms.Label();
-            this.CopyButton = new System.Windows.Forms.Button();
-            this.PositionsButton = new System.Windows.Forms.Button();
+            this.UpdateButton = new System.Windows.Forms.Button();
             this.gMap = new GMap.NET.WindowsForms.GMapControl();
             this.UpdateTimer = new System.Windows.Forms.Timer(this.components);
-            this.ShapesBttn = new System.Windows.Forms.Button();
+            this.TimerCheck = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
-            // MainFeed
+            // UpdateButton
             // 
-            this.MainFeed.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.MainFeed.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.MainFeed.Location = new System.Drawing.Point(12, 43);
-            this.MainFeed.Name = "MainFeed";
-            this.MainFeed.Size = new System.Drawing.Size(155, 658);
-            this.MainFeed.TabIndex = 1;
-            this.MainFeed.Text = "Feed goes here...";
-            // 
-            // CopyButton
-            // 
-            this.CopyButton.Location = new System.Drawing.Point(95, 12);
-            this.CopyButton.Name = "CopyButton";
-            this.CopyButton.Size = new System.Drawing.Size(72, 23);
-            this.CopyButton.TabIndex = 2;
-            this.CopyButton.Text = "Copy";
-            this.CopyButton.UseVisualStyleBackColor = true;
-            this.CopyButton.Click += new System.EventHandler(this.CopyButton_Click);
-            // 
-            // PositionsButton
-            // 
-            this.PositionsButton.Enabled = false;
-            this.PositionsButton.Location = new System.Drawing.Point(12, 12);
-            this.PositionsButton.Name = "PositionsButton";
-            this.PositionsButton.Size = new System.Drawing.Size(77, 23);
-            this.PositionsButton.TabIndex = 3;
-            this.PositionsButton.Text = "Update Map";
-            this.PositionsButton.UseVisualStyleBackColor = true;
-            this.PositionsButton.Click += new System.EventHandler(this.PositionsButton_Click);
+            this.UpdateButton.Location = new System.Drawing.Point(12, 12);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(77, 23);
+            this.UpdateButton.TabIndex = 3;
+            this.UpdateButton.Text = "Update Map";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
             // 
             // gMap
             // 
@@ -81,7 +57,7 @@
             this.gMap.GrayScaleMode = false;
             this.gMap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
             this.gMap.LevelsKeepInMemmory = 5;
-            this.gMap.Location = new System.Drawing.Point(173, 43);
+            this.gMap.Location = new System.Drawing.Point(12, 43);
             this.gMap.MarkersEnabled = true;
             this.gMap.MaxZoom = 18;
             this.gMap.MinZoom = 2;
@@ -95,49 +71,47 @@
             this.gMap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
             this.gMap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
             this.gMap.ShowTileGridLines = false;
-            this.gMap.Size = new System.Drawing.Size(648, 658);
+            this.gMap.Size = new System.Drawing.Size(1417, 1354);
             this.gMap.TabIndex = 5;
             this.gMap.Zoom = 10D;
             // 
             // UpdateTimer
             // 
+            this.UpdateTimer.Enabled = true;
             this.UpdateTimer.Interval = 10000;
             this.UpdateTimer.Tick += new System.EventHandler(this.UpdateTimer_Tick);
             // 
-            // ShapesBttn
+            // TimerCheck
             // 
-            this.ShapesBttn.Location = new System.Drawing.Point(173, 12);
-            this.ShapesBttn.Name = "ShapesBttn";
-            this.ShapesBttn.Size = new System.Drawing.Size(86, 23);
-            this.ShapesBttn.TabIndex = 6;
-            this.ShapesBttn.Text = "Make Shapes";
-            this.ShapesBttn.UseVisualStyleBackColor = true;
-            this.ShapesBttn.Click += new System.EventHandler(this.ShapesBttn_Click);
+            this.TimerCheck.AutoSize = true;
+            this.TimerCheck.Location = new System.Drawing.Point(95, 16);
+            this.TimerCheck.Name = "TimerCheck";
+            this.TimerCheck.Size = new System.Drawing.Size(90, 17);
+            this.TimerCheck.TabIndex = 6;
+            this.TimerCheck.Text = "Update Timer";
+            this.TimerCheck.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(833, 714);
-            this.Controls.Add(this.ShapesBttn);
+            this.ClientSize = new System.Drawing.Size(1441, 1410);
+            this.Controls.Add(this.TimerCheck);
             this.Controls.Add(this.gMap);
-            this.Controls.Add(this.PositionsButton);
-            this.Controls.Add(this.CopyButton);
-            this.Controls.Add(this.MainFeed);
+            this.Controls.Add(this.UpdateButton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Label MainFeed;
-        private System.Windows.Forms.Button CopyButton;
-        private System.Windows.Forms.Button PositionsButton;
+        private System.Windows.Forms.Button UpdateButton;
         private GMap.NET.WindowsForms.GMapControl gMap;
         private System.Windows.Forms.Timer UpdateTimer;
-        private System.Windows.Forms.Button ShapesBttn;
+        private System.Windows.Forms.CheckBox TimerCheck;
     }
 }
 
--- TransitFeeds/TransitFeeds/MetraData.cs
+++ TransitFeeds/TransitFeeds/MetraData.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -10,12 +14,20 @@ namespace TransitFeeds
 {
     class MetraData
     {
+        Form1 MainForm;
+
         string BaseCall = "https://gtfsapi.metrarail.com";
-        string PostionTail = "/gtfs/raw/positionUpdates.dat";
+        string PostionTail = "/gtfs/positions";
         string StopTail = "/gtfs/schedule/stops";
         string RouteTail = "/gtfs/schedule/shapes";
+        string RouteDetailsTail = "/gtfs/schedule/routes";
         string Tail = "";
 
+
+        public void Init(Form1 f)
+        {
+            MainForm = f;
+        }
 
 
         string GET(string url)
@@ -32,8 +44,153 @@ namespace TransitFeeds
             return StrmRdr.ReadToEnd();
         }
 
+        Dictionary<string, SortedDictionary<float, List<float>>> ShapeData = new Dictionary<string, SortedDictionary<float, List<float>>>();
+        Dictionary<string, List<float>> Positions = new Dictionary<string, List<float>>();
+        Dictionary<string, string> TrainNames = new Dictionary<string, string>();
+        Dictionary<string, List<float>> StopLocations = new Dictionary<string, List<float>>();
+        Dictionary<string, string> StopNames = new Dictionary<string, string>();
+        Dictionary<string, List<string>> StopZones = new Dictionary<string, List<string>>();
 
 
+        public void GetPositions()
+        {
+            string jsonData = GET(BaseCall + PostionTail);
+
+            Positions.Clear();            TrainNames.Clear();
+
+            dynamic result = Newtonsoft.Json.JsonConvert.DeserializeObject(jsonData);
+
+            foreach (var v in result)
+            {
+                string ID = v.vehicle.vehicle.id;
+                float Lat = v.vehicle.position.latitude; float Lon = v.vehicle.position.longitude;
+
+                string RouteID = v.vehicle.trip.route_id;
+                string VID = v.vehicle.vehicle.label;
+
+                List<float> list = new List<float>() { Lat, Lon };
+                Positions.Add(ID, list);
+                TrainNames.Add(ID, RouteID + " : " + VID);
+            }
+            GMapOverlay markersOverlay = new GMapOverlay("markers");
+            foreach (var v in Positions)
+            {
+                PointLatLng PLL = new PointLatLng(v.Value[0], v.Value[1]);
+                GMapMarker marker = new GMarkerGoogle(PLL, Properties.Resources.BlueRoundMarker);
+                marker.ToolTipText = TrainNames[v.Key];              
+                markersOverlay.Markers.Add(marker);
+            }
+            MainForm.OverlayTemp(markersOverlay);
+        }
+
+        Dictionary<string, string> StopUrlDict = new Dictionary<string, string>();
+        public void GetStops()
+        {
+            string jsonData = GET(BaseCall + StopTail);
+            StopLocations.Clear(); StopNames.Clear(); StopZones.Clear();
+            dynamic result = Newtonsoft.Json.JsonConvert.DeserializeObject(jsonData);
+            foreach (var v in result)
+            {
+                string ID = v.stop_id;
+                float Lat = v.stop_lat;
+                float Lon = v.stop_lon;
+                string name = v.stop_name;
+                string zone = v.zone_id;
+                string stopURL = v.stop_url;
+                List<float> list = new List<float>() { Lat, Lon };
+                StopLocations.Add(ID, list); StopNames.Add(ID, name); StopUrlDict.Add(ID, stopURL);
+                if (StopZones.ContainsKey(zone)) StopZones[zone].Add(ID);
+                else StopZones.Add(zone, new List<string>() { ID });
+            }
+            GMapOverlay stopsOverlay = new GMapOverlay("stops");
+
+            foreach (var stop in StopLocations)
+            {
+                PointLatLng PLL = new PointLatLng(stop.Value[0], stop.Value[1]);
+              //  GMapMarker marker = new GMarkerGoogle(PLL, Properties.Resources.RedSquareMarker);
+
+                foreach (var v in RouteDetailsDict)
+                {
+                    if (StopUrlDict[stop.Key].Contains(v.Key))
+                    {
+                        Bitmap Bmp = new Bitmap(10, 10);
+                        for (int h = 0; h < Bmp.Height; h++)
+                        {
+                            for (int w = 0; w < Bmp.Width; w++)
+                            {
+                                Bmp.SetPixel(w, h, ColorTranslator.FromHtml("#" + v.Value[1]));
+                            }
+                        }
+
+
+                        GMapMarker marker = new GMarkerGoogle(PLL,Bmp);
+                        marker.ToolTipText = StopNames[stop.Key];
+                        stopsOverlay.Markers.Add(marker);
+                    }
+                }
+
+
+
+
+
+
+            }
+            MainForm.OverlayPermant(stopsOverlay);
+        }
+
+        public void GetRoutes()
+        {
+            string jsonData = GET(BaseCall + RouteTail);
+            dynamic result = Newtonsoft.Json.JsonConvert.DeserializeObject(jsonData);
+            foreach (var v in result)
+            {
+                string ID = v.shape_id;
+                float SequenceNumber = v.shape_pt_sequence;
+                float LAT = v.shape_pt_lat; float LON = v.shape_pt_lon;
+                if (!ShapeData.ContainsKey(ID))                {                    ShapeData.Add(ID, new SortedDictionary<float, List<float>>() { { SequenceNumber, new List<float>() { LAT, LON } } });                }
+                else if (!ShapeData[ID].ContainsKey(SequenceNumber))                {                    ShapeData[ID].Add(SequenceNumber, new List<float>() { LAT, LON });                }
+            }            
+
+            GMapOverlay polygons = new GMapOverlay("polygons");
+            foreach (var shape in ShapeData)
+            {
+                List<PointLatLng> points = new List<PointLatLng>();
+                foreach (var point in shape.Value)
+                {
+                    points.Add(new PointLatLng(point.Value[0], point.Value[1]));
+                }
+                GMapRoute route = new GMapRoute(points, shape.Key);
+
+                route.Stroke = new Pen(Color.DarkBlue, 5);
+
+                foreach (var v in RouteDetailsDict)
+                {
+                    if (shape.Key.Contains(v.Key))
+                    {
+                        route.Stroke = new Pen(ColorTranslator.FromHtml("#" + v.Value[1]), 5);
+                    }
+                }
+
+                polygons.Routes.Add(route);
+            }
+            MainForm.OverlayPermant(polygons);
+
+        }
+
+        Dictionary<string, List<string>> RouteDetailsDict = new Dictionary<string, List<string>>();
+       public void RouteDetails()
+        {
+            string jsonData = GET(BaseCall + RouteDetailsTail);
+            dynamic result = Newtonsoft.Json.JsonConvert.DeserializeObject(jsonData);
+            foreach (var v in result)
+            {
+                string ID = v.route_id;
+                string Color = v.route_color;
+                string Name = v.route_long_name;
+                RouteDetailsDict.Add(ID, new List<string>() { Name, Color });
+            }
+
+        }
 
 
     }
